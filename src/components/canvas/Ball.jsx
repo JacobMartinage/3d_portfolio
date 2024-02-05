8fc43dc78ff739b07b9b419b5da26c5e7819e570
JacobMartinage <62039158+JacobMartinage@users.noreply.github.com>
2024-02-05 14:08:51 -0500

    more ball updates

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -13,7 +13,7 @@ const Ball = (props) => {
 
 
   return (
-    <Float speed = {1.21 } rotationIntensity={1} floatIntensity={2}>
+    <Float speed = {2} rotationIntensity={1.5} floatIntensity={2}>
       <ambientLight intensity={0.25} />
       <directionalLight position={[0, 0, 0.05]} />
       <mesh castShadow receiveShadow scale={2.5}>
