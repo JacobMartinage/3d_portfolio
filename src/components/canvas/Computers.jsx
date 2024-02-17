8a6bdc0a00d19a0424c72795ee6790f15bda4e36
JacobMartinage <62039158+JacobMartinage@users.noreply.github.com>
2024-02-17 17:59:16 -0500

    push iframe into tv during dev

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -23,7 +23,7 @@ const Computers = ( { isMobile } ) => {
         castShadow
         shadow-mapSize={1024}
       />
-      <mesh position={[0.525, -0.1, -.675]} rotation={[0,Math.PI,0]}> {/* Adjust position within screen */}
+      <mesh position={[0.525, -0.1, -.25]} rotation={[0,Math.PI,0]}> {/* Adjust position within screen */}
                 <planeGeometry args={[3.45, 3.2]} /> {/* Adjust size to fit screen */}
                 <Html 
                     transform 
